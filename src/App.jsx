4fa7ee05495d1f70d7b712c565be8eec8c865c7b
cyrilfassiau <cyril.fassiau@gmail.com>
2026-02-21 16:30:35 +0100

    Fix casing issue for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import Home from "./pages/home"
+import Home from "./pages/Home";
 import Features from "./pages/Features"
 import Header from "./components/Header"
 import Pricing from "./pages/Pricing"
